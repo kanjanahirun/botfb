54d57d0fcabf966c252470c9849c0456a8cfdefe
Jirayu <Jn_Jirayu@hotmail.com>
2016-04-24 14:38:11 +0700

    Update serverjs

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-// ver token = "CAAMlljgxbmQBAERXB6XphXjyDAnfkG2pzVI2zdhqZB1w91RKlnvPN4vRuZCroQ8m4bOTDhs0umU6aGcbgZAMSQ2ZCXYAyfC9k3xvEa2fk8KNKlwW38dS3mbcCWSS4OLrOPWRhzQJ1G0y7mrCsvw39QzgZBjxUzzdo6XfcWHKZBnBB7xZBCtSUqRkNZCuN9utYnQZD";
+var request = require('request');
+var token = "CAAMlljgxbmQBAERXB6XphXjyDAnfkG2pzVI2zdhqZB1w91RKlnvPN4vRuZCroQ8m4bOTDhs0umU6aGcbgZAMSQ2ZCXYAyfC9k3xvEa2fk8KNKlwW38dS3mbcCWSS4OLrOPWRhzQJ1G0y7mrCsvw39QzgZBjxUzzdo6XfcWHKZBnBB7xZBCtSUqRkNZCuN9utYnQZD";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 app.use(bodyParser.json());
 
 app.get('/', function (req, res){
@@ -31,7 +54,12 @@ app.post('/webhook/', function (req, res) {
       text = event.message.text;
       // Handle a text message from this sender
       console.log(text);
+
+      sendTextMessage(sender,text)
+
     }
   }
   res.sendStatus(200);
 });
+
+
