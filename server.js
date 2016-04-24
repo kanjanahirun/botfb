0f6802ff38ec92da75377496585ab7484a2bf2e3
Jirayu <Jn_Jirayu@hotmail.com>
2016-04-24 14:02:10 +0700

    Update serverjs

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function () {
 	console.log('Excample app listening on port' + app.get('port'));
 });
+
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === <jirayubot>) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');    
+  }
+});
