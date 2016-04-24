23b3d1f5c343a3ce85d24da6be439455231e1ff0
Jirayu <Jn_Jirayu@hotmail.com>
2016-04-24 15:04:06 +0700

    Update serverjs

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var token = "CAAMlljgxbmQBAERXB6XphXjyDAnfkG2pzVI2zdhqZB1w91RKlnvPN4vRuZCroQ8m4b
 
 function sendTextMessage(sender, text) {
   messageData = {
-    text:"Welcone to Natalie Action Camera"
+    text:"Welcone to Natalie Action Camera. Can I help you ?"
   }
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -63,56 +63,3 @@ app.post('/webhook/', function (req, res) {
 });
 
 
-function sendGenericMessage(sender) {
-  messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [{
-          "title": "First card",
-          "subtitle": "Element #1 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-          "buttons": [{
-            "type": "web_url",
-            "url": "https://www.messenger.com/",
-            "title": "Web url"
-          }, {
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for first element in a generic bubble",
-          }],
-        },{
-          "title": "Second card",
-          "subtitle": "Element #2 of an hscroll",
-          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-          "buttons": [{
-            "type": "postback",
-            "title": "Postback",
-            "payload": "Payload for second element in a generic bubble",
-          }],
-        }]
-      }
-    }
-  };
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
-}
-
-  if (text === 'Generic') {
-    sendGenericMessage(sender);
-    continue;
-  }
