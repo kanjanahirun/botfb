c54126328ada1fcae13b8d38cac72092730c964d
Jirayu <Jn_Jirayu@hotmail.com>
2016-04-24 12:41:32 +0700

    Update serverjs

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function (req, res){
+	res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function () {
+	console.log('Excample app listening on port' + app.get('port'));
 });
