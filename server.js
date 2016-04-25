e01480acab82ac1c300cf0630922ff1515f63d1a
Jirayu <Jn_Jirayu@hotmail.com>
2016-04-25 20:40:46 +0700

    Update serverjs

--- server.js
+++ server.js
@@ -1,12 +1,62 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
-var token = "CAAMlljgxbmQBAERXB6XphXjyDAnfkG2pzVI2zdhqZB1w91RKlnvPN4vRuZCroQ8m4bOTDhs0umU6aGcbgZAMSQ2ZCXYAyfC9k3xvEa2fk8KNKlwW38dS3mbcCWSS4OLrOPWRhzQJ1G0y7mrCsvw39QzgZBjxUzzdo6XfcWHKZBnBB7xZBCtSUqRkNZCuN9utYnQZD";
+var token = "CAAIaDUQwTC8BADTtSlCUX9mHLtqyJt5qHZCLN7dkoGSS8kOU6QZCkYBuwagX9FeZB8sveiiAJZA9bxjfJ2S7kvwUR0YuLDDlaXQkTu7HSQApsm5ThtIXvzZAig5WztZCYPZAAcp63nypyVx5v82AAOKqjEuR5VKpHKj9phZB0OVhrrVqbPitJcf4tscHn0cZBJZAYZD";
+var app = express();
+var check = 0;
+app.use(bodyParser.json());
 
-function sendTextMessage(sender, text) {
-  messageData = {
-    text:"สวัสดีครับ"
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'zonlyoner') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
+app.post('/webhook/', function (req, res) {
+  var messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    var event = req.body.entry[0].messaging[i];
+    var sender = event.sender.id;
+    if (event.message && event.message.text) {
+      var text = event.message.text;
+      // Handle a text message from this sender
+      console.log(text);
+      if (text == '...'){
+      	sendTextMessage(sender, "ว่าไง");
+      }
+      else if (text == 'อยากฟังเพลง'){
+      	sendTextMessage(sender, "เพลงอะไรค่ะ");
+      	check = 1;
+      }
+      else if (check == 1){
+      	sendTextMessage(sender, "https://www.youtube.com/results?search_query=" + text);
+      	check = 0;
+      }
+			else if (text == 'อยากดูรูป'){
+      	sendTextMessage(sender, "อยากให้หารูปเกี่ยวกับอะไรค่ะ");
+      	check = 2;
+      }
+      else if (check == 2){
+      	sendTextMessage(sender, "เสือกจังตะ https://www.google.co.th/search?q=" + text + "&source=lnms&tbm=isch");
+      	check = 0;
+      }
+      else {
+      	sendTextMessage(sender, "ดิฉันไม่เข้าใจที่คุณพิมพ์ค่ะ");
+      }
+    }
+  }
+  res.sendStatus(200);
+});
+
+function sendTextMessage(sender, text) { 
+  var messageData = {
+    text:text
   }
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -25,41 +75,6 @@ function sendTextMessage(sender, text) {
   });
 }
 
-app.use(bodyParser.json());
-
-app.get('/', function (req, res){
-	res.send('Hello World!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
-
-app.set('port', (process.env.PORT || 5000));
-
-app.listen(app.get('port'), function () {
-	console.log('Excample app listening on port' + app.get('port'));
-});
-
-app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === "jirayubot") {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Error, wrong validation token');    
-  }
-});
-
-app.post('/webhook/', function (req, res) {
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      // Handle a text message from this sender
-      console.log(text);
-
-      sendTextMessage(sender,text);
-
-    }
-  }
-  res.sendStatus(200);
-});
-
-
